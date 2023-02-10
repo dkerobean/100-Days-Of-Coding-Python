de95ac81cff84391082c6d2ac9cc2f890ae28d00
dickson <dkerobean@gmail.com>
2023-02-10 18:42:07 +0000

    Day 25 2

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,59 +1,58 @@
-
-import csv
+from turtle import Turtle, Screen
 import pandas
 
-# with open("weather-data.csv") as data:
-#     data_file = csv.reader(data)
-#     temperatures = []
-#
-#     for row in data_file:
-#         temperatures.append(int(row[1]))
-#
-#     print(temperatures)
+turtle = Turtle()
+screen = Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
 
-data = pandas.read_csv("weather-data.csv")
+guessed_states = []
 
-# convert to dictionart
-data_dict = data.to_dict()
-print(data_dict)
+while len(guessed_states) < 50:
+    user_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct", prompt="What is another state").title()
+    all_states = data["state"].to_list()
 
-# convert to list
-temp_list = data["temp"].to_list()
-print(temp_list)
+    if user_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
 
-# average temp
-avg_temp = sum(temp_list) / len(temp_list)
-print(avg_temp)
+    if user_state in all_states:
+        guessed_states.append(user_state)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data["state"] == user_state]
+        t.goto(int(state_data["x"]), int(state_data["y"]))
+        t.write(user_state)
 
-# get data in row
-print(data[data["day"] == "Monday"])
 
-# get the highest temp row
-# highest_temp = 0
-# for temp in temp_list:
-#     if temp > highest_temp:
-#         highest_temp = temp
-#
-# print(data[data["temp"] == highest_temp])
 
-print(data[data["temp"] == data["temp"].max()])
 
-monday = data[data["day"] == "Monday"]
-print(monday.condition)
 
-#convert monday temp to farenheight
-monday_temp = int(monday.temp)
-print(monday_temp * 9/5 + 32)
 
-#create a dataframe from scratch
 
-data_dict = {
-    "students": ["Amy", "Dickson", "Lord"],
-    "scores": [34, 56, 67]
-}
 
-new_data = pandas.DataFrame(data_dict)
-new_data.to_csv("new_data.csv")
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
+
 
 
 
