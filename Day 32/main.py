7645f0533c2c845d00104395d351ba8599ba5739
dickson <dkerobean@gmail.com>
2023-02-21 17:03:36 +0000

    Day 32 DateTimeModule

---
+++ Day 32/main.py
@@ -0,0 +1,11 @@
+import datetime as dt
+
+now = dt.datetime.now()
+
+year = now.year
+month = now.month
+minute = now.minute
+day_of_the_week = now.weekday()
+
+dob = dt.datetime(year=1997, month=9, day=27)
+
