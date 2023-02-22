5bdf9ff914954b8f8d117445949b8f62d5fead8d
dickson <dkerobean@gmail.com>
2023-02-22 12:50:25 +0000

    Day 33 iss_position and email project

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,21 +1,31 @@
 import requests
+import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 5.603717
+MY_LNG = -0.186964
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters,)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-iss_position = (longitude, latitude)
-print(iss_position)
 
-# exercise
 
-def get_quote():
-    response = requests.get(url="https://api.kanye.rest")
-    data = response.json()
-    quote = data["quote"]
-    print(quote)
-
-get_quote()
+time_now = dt.datetime.now()
